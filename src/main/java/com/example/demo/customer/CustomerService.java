f4b64e23cf52587990f8d39f46ac23aafca94611
murtada abd algalil <murtada199815@gmail.com>
2023-11-21 11:54:22 +0200

    add micrometer

--- src/main/java/com/example/demo/customer/CustomerService.java
+++ src/main/java/com/example/demo/customer/CustomerService.java
@@ -2,23 +2,38 @@ package com.example.demo.customer;
 
 
 import com.example.demo.exception.NotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.util.List;
 @Service
 public class CustomerService {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(
+            CustomerService.class
+    );
     private final CustomerRepository customerRepository;
 @Autowired
     public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
     List<Customer> getCustomers(){
+    LOGGER.info("getCustomers call in here");
     return  customerRepository.findAll();
 }
 
 
     Customer getCustomer(long id){
         return customerRepository.findById(id).
-                orElseThrow(()->new NotFoundException("cudtomer with id "+ id +" not found "));
+                orElseThrow(
+                        ()->
+                        {
+                            NotFoundException notFoundException =new NotFoundException(
+                                    "customer with id "+id +"not found");
+                                    LOGGER.error("error in getting customer {}",id ,notFoundException);
+                            return notFoundException;
+
+                        });
     }
 }
